27dd8e656afea26ac38e5d1415bad8707ad8adbf
Inuoluwadunsimi <danielolaoladeinde@gmail.com>
2024-04-01 15:27:19 +0100

    chore: updated swagger config

--- src/configuration/swagger.ts
+++ src/configuration/swagger.ts
@@ -1,16 +1,19 @@
-import swaggerJSDoc from "swagger-jsdoc";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const swaggerJSDoc = require("swagger-jsdoc");
 
+// options for the swagger docs
 const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "Beauty-butler interview",
-      version: "1.0.0",
+      title: "Glouse API Docs", // Title of the documentation
+      version: "1.0.0", // Version of the app
       description: "Docs",
     },
   },
-
+  // swaggerDefinition,
   apis: ["./spec/api.yaml"],
 };
 
-export default swaggerJSDoc(options);
+// initialize swagger-jsdoc
+module.exports = swaggerJSDoc(options);
